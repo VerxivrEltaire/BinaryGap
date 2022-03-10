0fcf5feefab448edfb1cc9c5de90f29736a1e03e
VerxivrEltaire <kenneth.iheme@gmail.com>
2022-03-10 08:44:23 +0100

    Project Commit

---
+++ BinaryGap/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryGap {
+
+    class Solution {
+        public int solution(int N) {
+            int value = 0;
+            var results = new List<int>();
+            var binary = Convert.ToString(N, 2);
+
+            foreach (var c in binary) {
+                if (c.Equals('0')) {
+                    value += 1;
+                } else {
+                    results.Add(value);
+                    value = 0;
+                }
+            }
+
+            return results.Max();
+        }
+    }
+
+    class MainClass {
+        public static void Main(string[] args) {
+            Solution sol = new Solution();
+            Console.WriteLine(sol.solution(1041));
+        }
+    }
+}
